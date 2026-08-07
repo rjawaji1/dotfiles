aae9ad77b2acabe978485f9ae24f253c5fdaa245
Rajeev Athreya Jawaji <contact@rjawaji.com>
2026-08-07 20:43:42 +1000

    move commands aliases and commands to interactive mode in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,38 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	starship init fish | source
+	tv init fish | source
+	mise activate fish | source
+
+	zoxide init fish | source
+
+	alias v="nvim"
+	alias vi="nvim"
+	alias ls="eza --group-directories-first"
+	alias lsa="eza --group-directories-first -l"
+	alias cat="bat -p"
+
+	alias :q="exit"
+
+	alias ga="git add"
+	alias gc="git commit"
+	alias gp="git push"
+	alias gs="git status"
+
+	alias ..="cd .."
+	alias ...="cd ../.."
+	alias ....="cd ../../.."
+	alias .....="cd ../../../.."
+	alias ......="cd ../../../../.."
+	alias .......="cd ../../../../../.."
+	alias ........="cd ../../../../../../.."
+	alias .........="cd ../../../../../../../.."
+	alias ..........="cd ../../../../../../../../.."
+	alias ...........="cd ../../../../../../../../../.."
 end
 
 set -gx EDITOR nvim
+set -gx VISUAL nvim
 
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH $HOME/.local/bin $PATH
@@ -12,29 +42,6 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 
 set -gx PATH $HOME/Library/Application\x20Support/Herd/bin $PATH
 
-alias v="nvim"
-alias vi="nvim"
-alias ls="eza --group-directories-first"
-alias lsa="eza --group-directories-first -l"
-alias cat="bat -p"
-
-alias :q="exit"
-
-alias ga="git add"
-alias gc="git commit"
-alias gp="git push"
-alias gs="git status"
-
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias ......="cd ../../../../.."
-alias .......="cd ../../../../../.."
-alias ........="cd ../../../../../../.."
-alias .........="cd ../../../../../../../.."
-alias ..........="cd ../../../../../../../../.."
-alias ...........="cd ../../../../../../../../../.."
 
 # Postgres
 set -gx PATH /opt/homebrew/opt/libpq/bin $PATH
@@ -42,9 +49,6 @@ set -gx PATH /opt/homebrew/opt/libpq/bin $PATH
 set -gx LDFLAGS -L/opt/homebrew/opt/libpq/lib $LDFLAGS
 set -gx CPPFLAGS -I/opt/homebrew/opt/libpq/include $CPPFLAGS
 
-starship init fish | source
-zoxide init fish | source
-mise activate fish | source
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
